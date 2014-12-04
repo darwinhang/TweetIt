d111b3a5143e2f6fbbdc99254146c5bef35eecb7
winious <darwin.hang@gmail.com>
2014-12-04 01:39:29 -0600

    Some notes and fiddling around

--- content.js
+++ content.js
@@ -4,16 +4,18 @@ console.log("what");
 
 // add tooltip, may need to float it
 function createTooltip(coordinates, parentEl) {
-	var newDiv = document.createElement('div');
-	newDiv.style.backgroundColor = 'blue';
-	newDiv.style.height = '30px';
-	newDiv.style.width = '30px';
-	newDiv.style.top = coordinates.bottom + 'px';
+	var newDiv = document.createElement('span');
+	// newDiv.style.backgroundColor = 'blue';
+	// newDiv.style.height = '30px';
+	// newDiv.style.width = '30px';
+	newDiv.style.bottom = coordinates.bottom + 'px';
 	newDiv.style.left = coordinates.left + 'px';
 	newDiv.style.zIndex = 30;
 	newDiv.setAttribute('title', 'what up?');
-	//newDiv.style.cssFloat = 'left';
+	newDiv.style.cssFloat = 'right';
 	newDiv.style.position = 'absolute';
+
+	console.log(parentEl);
 
 	parentEl.appendChild(newDiv);
 	console.log('what up')
@@ -40,14 +42,32 @@ function selected() {
 		return;
 	} 
 	else {
-		var objRange = highlighted.getRangeAt();
+		//var objRange = highlighted.getRangeAt();
+		// trying a method mentioned in stack overflow
+		var range = window.getSelection().getRangeAt();
+		var dummy = document.createElement("span");
+		range.insertNode(dummy);
+		var what = range.getBoundingClientRect();
+		var left = what.left, bottom = what.bottom;
+		dummy.parentNode.removeChild(dummy);
 		console.log('quote:', highlightedText)
 		console.log('page:', currentPage)
-		console.log('position', objRange)
-		console.log('bounding client', getCoords(objRange))
+		// console.log('position', objRange)
+		// console.log('bounding client', getCoords(objRange))
+		console.log('position', range)
+		console.log('bounding client', getCoords(range))
 
-		createTooltip(getCoords(objRange), objRange.commonAncestorContainer);
+		// createTooltip(getCoords(objRange), objRange.commonAncestorContainer);
+		createTooltip({'left': left, 'bottom': bottom}, range.commonAncestorContainer);
 	}
 }
+
+// // trying a method mentioned in stack overflow
+// var range = window.getSelection().getRangeAt(0);
+// var dummy = document.createElement("span");
+// range.insertNode(dummy);
+// var box = document.getBoxObjectFor(dummy);
+// var x = box.x, y = box.y;
+// dummy.parentNode.removeChild(dummy);
 
 document.body.addEventListener('mouseup' , selected);
--- notes.md
+++ notes.md
@@ -8,3 +8,20 @@ The goal is still to have this work like the action that happens when a user hig
 I'm having trouble creating a tooltip over the highlighted text.
 
 Okay, I think the problem was that I was not using absolute positioning.
+
+Well, that didn't really solve much. Looks like I need to use float and relative positioning. I need to find a way to get the parent element of the leftmost highlighted text. For example, if the highlighted text spans multiple list item elements, I always want the div placed, well, I don't know yet.
+
+Another issue I'm having is that
+
+### 12/4/2014 
+I guess it is already the 4th. It looks like I can use stylesheets, so I don't have to go through all the trouble of styling using JS. http://stackoverflow.com/questions/11553600/how-to-inject-css-using-content-script-file-in-chrome-extension 
+
+I should also probably move to jQuery, since I'll probably also be using a jQuery tooltip library: 
+http://iamceege.github.io/tooltipster/#getting-started
+
+If I use a tooltip plugin, I might be better off adding a span element around the first highlighted word. 
+
+Damn, this might have been the answer I've been looking for:
+http://stackoverflow.com/questions/16498877/how-to-wrap-highlighted-text-with-a-span-and-place-a-div-positioned-relative-to 
+
+except, I won't be using jQueryUI and I don't want to highlight the whole selection, just the first word.
