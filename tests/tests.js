df6505a1d264687b73143ffaa8a299d04f9edddd
winious <darwin.hang@gmail.com>
2015-02-01 14:20:11 -0600

    Extract core functionality to module

--- tests/tests.js
+++ tests/tests.js
@@ -25,3 +25,23 @@ describe('quoteLength function', function(){
 		expect(sameQuote).toEqual(quote);
 	});
 });
+
+describe('quoteLength function as property of TweetIt module', function(){
+	beforeEach(function(){
+		var newTweet = Object.create(TweetIt);
+	});
+	it('should be defined', function(){
+		expect(TweetIt.limitQuote).toBeDefined();
+	})
+	// ensure lenght of strings are under 100
+	it('should restrict string lengths to 100', function(){
+		var quote = 'this is an insanely string but what you do know about the things to come from the future, okay that is weird';
+		expect(limitQuote(quote).length).toBe(100);
+	});
+	// if string is already less than 100, the string should be returned
+	it('should return the string', function(){
+		var quote = 'what is up';
+		var sameQuote = limitQuote(quote);
+		expect(sameQuote).toEqual(quote);
+	});
+});
